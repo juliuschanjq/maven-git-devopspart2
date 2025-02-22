b1ee1054d51e9faba7f29b8f598bd580faa01bb0
Julius <juliuschanjq@gmail.com>
2025-02-22 17:53:06 +0800

    Commit Changes to Pages

---
+++ src/main/java/com/dvops/maven/eclipse/contactDetailsTest.java
@@ -0,0 +1,15 @@
+package com.dvops.maven.eclipse;
+
+import junit.framework.TestCase;
+
+public class contactDetailsTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
